3e0ce2a66865fe9b03fa43452158e3f0866b7e73
Henrique-RMotta <henrique2008790@gmail.com>
2025-10-12 20:05:49 -0300

    fix: organização

--- a.js
+++
@@ -1,8 +0,0 @@
-const agora = new Date;
-console.log(agora.getDate());
-
-let a = [1,2,3,4]
-a.join("");
-
-let mes = agora.getMonth();
-console.log(mes);
--- mandararq.py
+++ mandararq.py
@@ -1,13 +1,13 @@
 import os 
 
 user_input = input("Qual arquivo deseja mandar \n1-Server\n2-Script\n3-HTML\n4-todos\n5-css\n")
-server = "scp C:/Users/tcc/server.js mottaaryana@192.168.0.97:/home/mottaaryana/tcc"
-script = "scp C:/Users/tcc/script.js mottaaryana@192.168.0.97:/home/mottaaryana/tcc/HTML"
-html = "scp C:/Users/tcc/HTML/menu.html mottaaryana@192.168.0.97:/home/mottaaryana/tcc/HTML"
-html2 = "scp C:/Users/tcc/HTML/página_inicial.html mottaaryana@192.168.0.97:/home/mottaaryana/tcc/HTML"
-html3 = "scp C:/Users/tcc/HTML/configurar.html mottaaryana@192.168.0.97:/home/mottaaryana/tcc/HTML"
-html4 = "scp C:/Users/tcc/HTML/verhorarios.html mottaaryana@192.168.0.97:/home/mottaaryana/tcc/HTML"
-html5 = "scp C:/Users/tcc/HTML/programar.html mottaaryana@192.168.0.97:/home/mottaaryana/tcc/HTML"
+server = "scp C:/Users/tcc/Assets/server/server.js mottaaryana@192.168.0.97:/home/mottaaryana/tcc"
+script = "scp C:/Users/tcc/Assets/script/script.js mottaaryana@192.168.0.97:/home/mottaaryana/tcc/HTML"
+html = "scp C:/Users/tcc/Assets/HTML/menu.html mottaaryana@192.168.0.97:/home/mottaaryana/tcc/HTML"
+html2 = "scp C:/Users/tcc/Assets/HTML/página_inicial.html mottaaryana@192.168.0.97:/home/mottaaryana/tcc/HTML"
+html3 = "scp C:/Users/tcc/Assets/HTML/configurar.html mottaaryana@192.168.0.97:/home/mottaaryana/tcc/HTML"
+html4 = "scp C:/Users/tcc/Assets/HTML/verhorarios.html mottaaryana@192.168.0.97:/home/mottaaryana/tcc/HTML"
+html5 = "scp C:/Users/tcc/AssetsHTML/programar.html mottaaryana@192.168.0.97:/home/mottaaryana/tcc/HTML"
 #obs: lembre-se de trocar o ip caso o rasp esteja conectado a rede do celular
 if user_input == '1':
     os.system(server)
